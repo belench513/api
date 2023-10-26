ac447968f986218e6bc226ee8bf8f5a023be9da3
Belen Chiodini <140737516+belench513@users.noreply.github.com>
2023-10-26 11:53:53 -0300

    script.js

--- script.js
+++ script.js
@@ -1,34 +1,28 @@
-const apiKey = '340d8281ffmsh1403e8250414e0dp1f3989jsn568e69117290';
-const url = 'https://pokedex2.p.rapidapi.com/pokedex/uk';
 
-async function searchPokemon() {
-    const searchInput = document.getElementById('searchInput');
-    const pokemonInfo = document.getElementById('pokemonInfo');
-    
-    try {
-        const response = await fetch(`${url}/${searchInput.value}`, {
-            method: 'GET',
-            headers: {
-                'X-RapidAPI-Key': apiKey,
-                'X-RapidAPI-Host': 'pokedex2.p.rapidapi.com'
-            }
+    // Función para hacer la solicitud a la PokeAPI
+    function getPokemonData(pokemonName) {
+      fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName.toLowerCase()}`)
+        .then(response => response.json())
+        .then(data => {
+          // Manejar la información recibida
+          displayPokemonInfo(data);
+        })
+        .catch(error => {
+          console.error('Error fetching data:', error);
         });
-        const data = await response.json();
-        
-        if (data && data[0]) {
-            const pokemon = data[0];
-            const pokemonHTML = `
-                <h2>${pokemon.name}</h2>
-                <img src="${pokemon.imageurl}">
-                <p>Type: ${pokemon.type.join(', ')}</p>
-                <p>Abilities: ${pokemon.abilities.join(', ')}</p>
-                <p>Base Experience: ${pokemon.base_experience}</p>
-            `;
-            pokemonInfo.innerHTML = pokemonHTML;
-        } else {
-            pokemonInfo.innerHTML = '<p>Pokémon no encontrado</p>';
-        }
-    } catch (error) {
-        console.error('Error al buscar Pokémon:', error);
     }
-}
+
+    // Función para mostrar la información del Pokémon
+    function displayPokemonInfo(data) {
+      const pokemonInfoDiv = document.getElementById('pokemon-info');
+      pokemonInfoDiv.innerHTML = `
+        <h2>${data.name}</h2>
+        <img src="${data.sprites.front_default}" alt="${data.name}">
+        <p>Height: ${data.height} dm</p>
+        <p>Weight: ${data.weight} hg</p>
+      `;
+    }
+
+    // Ejemplo de uso con un Pokémon específico (puedes cambiar el nombre)
+    getPokemonData('pikachu');
+  
